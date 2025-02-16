1af795b8d0dbd7fb1d15d3245151614b517df37c
Ryan Belgrave <ryan@belgrave.me>
2025-02-16 09:33:35 -0600

    forgot to save postgres

--- openstack-control-plane.tf
+++ openstack-control-plane.tf
@@ -1,54 +1,54 @@
-# Postgres
-module "openstack-postgres-1" {
-  source       = "./modules/vm"
-  name         = "openstack-postgres-1.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_hardened
-  datastore_id = local.freenas_nfs_datastore
+# # Postgres
+# module "openstack-postgres-1" {
+#   source       = "./modules/vm"
+#   name         = "openstack-postgres-1.us-homelab1.hl.rmb938.me"
+#   image_family = local.family_ubuntu_noble_lts_amd64_hardened
+#   datastore_id = local.freenas_nfs_datastore
 
-  network_device_bridge = "vmbr0v23"
-  ip_config_ipv4 = {
-    address = "192.168.23.70/${local.vmbr0v23_cidr}"
-    gateway = local.vmbr0v23_gateway
-  }
+#   network_device_bridge = "vmbr0v23"
+#   ip_config_ipv4 = {
+#     address = "192.168.23.70/${local.vmbr0v23_cidr}"
+#     gateway = local.vmbr0v23_gateway
+#   }
 
-  cpu              = 1
-  memory           = 2 * 1024
-  additional_disks = [100]
-  replacement      = 1
-}
+#   cpu              = 1
+#   memory           = 2 * 1024
+#   additional_disks = [100]
+#   replacement      = 1
+# }
 
-module "openstack-postgres-2" {
-  source       = "./modules/vm"
-  name         = "openstack-postgres-2.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_hardened
-  datastore_id = local.freenas_nfs_datastore
+# module "openstack-postgres-2" {
+#   source       = "./modules/vm"
+#   name         = "openstack-postgres-2.us-homelab1.hl.rmb938.me"
+#   image_family = local.family_ubuntu_noble_lts_amd64_hardened
+#   datastore_id = local.freenas_nfs_datastore
 
-  network_device_bridge = "vmbr0v23"
-  ip_config_ipv4 = {
-    address = "192.168.23.71/${local.vmbr0v23_cidr}"
-    gateway = local.vmbr0v23_gateway
-  }
+#   network_device_bridge = "vmbr0v23"
+#   ip_config_ipv4 = {
+#     address = "192.168.23.71/${local.vmbr0v23_cidr}"
+#     gateway = local.vmbr0v23_gateway
+#   }
 
-  cpu              = 1
-  memory           = 2 * 1024
-  additional_disks = [100]
-  replacement      = 1
-}
+#   cpu              = 1
+#   memory           = 2 * 1024
+#   additional_disks = [100]
+#   replacement      = 1
+# }
 
-module "openstack-postgres-3" {
-  source       = "./modules/vm"
-  name         = "openstack-postgres-3.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_hardened
-  datastore_id = local.freenas_nfs_datastore
+# module "openstack-postgres-3" {
+#   source       = "./modules/vm"
+#   name         = "openstack-postgres-3.us-homelab1.hl.rmb938.me"
+#   image_family = local.family_ubuntu_noble_lts_amd64_hardened
+#   datastore_id = local.freenas_nfs_datastore
 
-  network_device_bridge = "vmbr0v23"
-  ip_config_ipv4 = {
-    address = "192.168.23.72/${local.vmbr0v23_cidr}"
-    gateway = local.vmbr0v23_gateway
-  }
+#   network_device_bridge = "vmbr0v23"
+#   ip_config_ipv4 = {
+#     address = "192.168.23.72/${local.vmbr0v23_cidr}"
+#     gateway = local.vmbr0v23_gateway
+#   }
 
-  cpu              = 1
-  memory           = 2 * 1024
-  additional_disks = [100]
-  replacement      = 1
-}
+#   cpu              = 1
+#   memory           = 2 * 1024
+#   additional_disks = [100]
+#   replacement      = 1
+# }
