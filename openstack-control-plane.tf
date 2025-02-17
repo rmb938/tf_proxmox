8be3f3141d82d5ca3a376cf4592c3f01b159ba85
Ryan Belgrave <ryan@belgrave.me>
2025-02-16 22:21:14 -0600

    Update openstack-control-plane.tf

--- openstack-control-plane.tf
+++ openstack-control-plane.tf
@@ -2,7 +2,7 @@
 module "openstack-postgres-1" {
   source       = "./modules/vm"
   name         = "openstack-postgres-1.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_hardened
+  image_family = "ubuntu-noble-lts-amd64-application"
   datastore_id = local.freenas_nfs_datastore
 
   network_device_bridge = "vmbr0v23"
@@ -14,7 +14,7 @@ module "openstack-postgres-1" {
   cpu              = 1
   memory           = 2 * 1024
   additional_disks = [100]
-  replacement      = 1
+  replacement      = 2
 }
 
 module "openstack-postgres-2" {
