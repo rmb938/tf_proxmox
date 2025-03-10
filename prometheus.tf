095112088d6d24296121cec188ab9b1f7db7afe8
Ryan Belgrave <ryan@belgrave.me>
2025-03-10 18:37:30 -0500

    remove all except elastic cause I didn't backup vault

--- prometheus.tf
+++ prometheus.tf
@@ -1,31 +1,31 @@
-locals {
-  # TODO: move the CONSUL_ROLE into the prometheus image
-  prometheus_cloud_config = <<-EOF
-runcmd:
-  - /usr/bin/systemctl set-environment CONSUL_ROLE=prometheus
-  - /usr/bin/echo -e "[Manager]\nDefaultEnvironment=CONSUL_ROLE=prometheus" | /usr/bin/tee /etc/systemd/system.conf.d/consul_role.conf
-  - /usr/bin/systemctl enable consul-template-consul.service
-  - /usr/bin/systemctl start consul-template-consul.service
-EOF
-}
+# locals {
+#   # TODO: move the CONSUL_ROLE into the prometheus image
+#   prometheus_cloud_config = <<-EOF
+# runcmd:
+#   - /usr/bin/systemctl set-environment CONSUL_ROLE=prometheus
+#   - /usr/bin/echo -e "[Manager]\nDefaultEnvironment=CONSUL_ROLE=prometheus" | /usr/bin/tee /etc/systemd/system.conf.d/consul_role.conf
+#   - /usr/bin/systemctl enable consul-template-consul.service
+#   - /usr/bin/systemctl start consul-template-consul.service
+# EOF
+# }
 
-module "prometheus" {
-  source       = "./modules/vm"
-  name         = "prometheus.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_application
-  datastore_id = local.freenas_nfs_datastore
+# module "prometheus" {
+#   source       = "./modules/vm"
+#   name         = "prometheus.us-homelab1.hl.rmb938.me"
+#   image_family = local.family_ubuntu_noble_lts_amd64_application
+#   datastore_id = local.freenas_nfs_datastore
 
-  network_device_bridge = "vmbr0v23"
-  ip_config_ipv4 = {
-    address = "192.168.23.58/${local.vmbr0v23_cidr}"
-    gateway = local.vmbr0v23_gateway
-  }
+#   network_device_bridge = "vmbr0v23"
+#   ip_config_ipv4 = {
+#     address = "192.168.23.58/${local.vmbr0v23_cidr}"
+#     gateway = local.vmbr0v23_gateway
+#   }
 
-  cpu              = 1
-  memory           = 2 * 1024
-  additional_disks = [100]
-  replacement      = 2
+#   cpu              = 1
+#   memory           = 2 * 1024
+#   additional_disks = [100]
+#   replacement      = 2
 
-  cloud_config = local.prometheus_cloud_config
-}
+#   cloud_config = local.prometheus_cloud_config
+# }
 
