41684a54d02f923e81858d1009eb9e7a78361533
Ryan Belgrave <ryan@belgrave.me>
2025-03-14 16:44:16 -0500

    haproxy new image

--- haproxy.tf
+++ haproxy.tf
@@ -1,13 +1,13 @@
 locals {
-  # TODO: move the CONSUL_ROLE into the haproxy-t1 image
   haproxy_t1_cloud_config = <<-EOF
-runcmd:
-  - /usr/bin/systemctl set-environment CONSUL_ROLE=haproxy-t1
-  - /usr/bin/echo -e "[Manager]\nDefaultEnvironment=CONSUL_ROLE=haproxy-t1" | /usr/bin/tee /etc/systemd/system.conf.d/consul_role.conf
+bootcmd:
+  - /usr/bin/echo "CONSUL_ROLE=haproxy-t1" >> /etc/cloud-environment
+  - /usr/bin/echo "HAPROXY_HTTP_BACKEND_CONSUL_SERVICE=haproxy-t2-http" >> /etc/cloud-environment
+  - /usr/bin/echo "HAPROXY_HTTPS_BACKEND_CONSUL_SERVICE=haproxy-t2-https" >> /etc/cloud-environment
+  - /usr/bin/echo "VIP_ROUTER_ID=51" >> /etc/cloud-environment
+  - /usr/bin/echo "VIP_ADDRESS=192.168.23.46/24" >> /etc/cloud-environment
   # Overwrite hostname since HAProxy doesn't exist yet
   - /usr/bin/echo "VAULT_ADDR=https://hashi-vault-$(/usr/bin/hostname | /usr/bin/tail -c 2).us-homelab1.hl.rmb938.me:8200" | /usr/bin/tee /etc/vault.d/vault.env
-  - /usr/bin/systemctl enable consul-template-consul.service
-  - /usr/bin/systemctl start consul-template-consul.service
 EOF
 }
 
@@ -27,7 +27,7 @@ EOF
 module "haproxy-t1-1" {
   source       = "./modules/vm"
   name         = "haproxy-t1-1.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_application
+  image_family = "ubuntu-noble-lts-amd64-haproxy-t1"
   datastore_id = local.freenas_nfs_datastore
 
   network_device_bridge = "vmbr0v23"
@@ -38,7 +38,7 @@ module "haproxy-t1-1" {
 
   cpu         = 1
   memory      = 2 * 1024
-  replacement = 3
+  replacement = 4
 
   cloud_config = local.haproxy_t1_cloud_config
 }
@@ -46,7 +46,7 @@ module "haproxy-t1-1" {
 module "haproxy-t1-2" {
   source       = "./modules/vm"
   name         = "haproxy-t1-2.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_application
+  image_family = "ubuntu-noble-lts-amd64-haproxy-t1"
   datastore_id = local.freenas_nfs_datastore
 
   network_device_bridge = "vmbr0v23"
@@ -57,7 +57,7 @@ module "haproxy-t1-2" {
 
   cpu         = 1
   memory      = 2 * 1024
-  replacement = 3
+  replacement = 4
 
   cloud_config = local.haproxy_t1_cloud_config
 }
