0573a24667502fcbf87d337bcff68cda1c4a7d67
Ryan Belgrave <ryan@belgrave.me>
2025-04-17 15:59:39 -0500

    Update openstack-control-plane.tf

--- openstack-control-plane.tf
+++ openstack-control-plane.tf
@@ -367,7 +367,7 @@ EOF
 module "openstack-ovn-northd-1" {
   source       = "./modules/vm"
   name         = "openstack-ovn-northd-1.us-homelab1.hl.rmb938.me"
-  image_family = local.family_ubuntu_noble_lts_amd64_application # TODO:
+  image_family = "ubuntu-noble-lts-amd64-openstack-ovn-northd"
   datastore_id = local.freenas_nfs_datastore
 
   network_device_bridge = "vmbr0v23"
