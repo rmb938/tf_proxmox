5ec31eb60c1471eb222bc353f59c666491afdb5e
Ryan Belgrave <ryan@belgrave.me>
2025-05-02 08:43:14 -0500

    fix image

--- openstack-compute.tf
+++ openstack-compute.tf
@@ -8,7 +8,7 @@ EOF
 module "openstack-compute-1" {
   source       = "./modules/vm"
   name         = "openstack-compute-1.us-homelab1.hl.rmb938.me"
-  image_family = "ubuntu-noble-lts-amd64-openstack-application"
+  image_family = local.family_ubuntu_noble_lts_amd64_application
   datastore_id = local.freenas_nfs_datastore
 
   network_device_bridge = "vmbr0v23"
